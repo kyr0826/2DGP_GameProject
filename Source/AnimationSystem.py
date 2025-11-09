c682a5fbee84bf0f17a8fe2957e14649a9d0d363
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-09 23:42:55 +0900

    애니메이션 시스템 구현

---
+++ Source/AnimationSystem.py
@@ -0,0 +1,62 @@
+from pico2d import Image
+from StateMachine import *
+
+class Animation:
+    def __init__(self, owner, frames : Image, frame_count, fps, loop=True):
+        self.owner = owner
+        self.frames = frames
+        self.frame_count = frame_count
+        self.fps = fps
+        self.loop = loop
+        self.time = 0
+        self.index = 0
+        self.isEnd = False
+
+    def update(self, delta_time):
+        if not self.loop and self.isEnd:
+            return
+
+        self.time += delta_time
+        frame_duration = 1.0 / self.fps
+        if self.time > frame_duration:
+            self.time -= frame_duration
+            if self.loop:
+                self.index = (self.index+1) % self.frame_count
+            else:
+                if self.index < self.frame_count-1:
+                    self.index += 1
+                else:
+                    self.index = self.frame_count-1
+                    self.isEnd = True
+    # 128 * 128
+    def draw_frame(self):
+        left = 0 + 128 * self.index
+        fliped = '' if self.owner.last_dir > 0 else 'h'
+        x, y = self.owner.pos_x, self.owner.pos_y
+        if self.owner.state_machine.current == States.RUN and self.owner.name != 'Fighter' and self.owner.x_speed != 0:
+            x += self.owner.last_dir * 16
+        self.frames.clip_composite_draw(left, 0, 128, 128, 0, fliped,x, y,128,128)
+
+
+class Animator:
+    def __init__(self,owner):
+        self.owner = owner
+        self.animations = {}
+        self.current = None
+
+    def add(self, state, animation):
+        self.animations[state] = animation
+
+    def play(self, state):
+        if state in self.animations:
+            self.current = self.animations[state]
+            self.current.index = 0
+            self.current.time = 0
+            self.current.isEnd = False
+
+    def update(self, delta_time):
+        if self.current:
+            self.current.update(delta_time)
+
+    def draw_current_frame(self):
+        self.current.draw_frame()
