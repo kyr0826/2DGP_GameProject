ea9e2bb90842f9c18a1574bd0b6d534c67ef4bf0
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-09 23:43:09 +0900

    맵 그리기 위한 모듈 구현

---
+++ Source/MapGenerator.py
@@ -0,0 +1,77 @@
+from pico2d import *
+from collections import namedtuple
+
+# 플랫폼 데이터를 저장할 구조체 정의 (사각형, 색상, 타입)
+# type ground (통과 불가) one-way (아래에서 위로 통과 가능)
+PlatformData = namedtuple('PlatformData', ['rect', 'color', 'type'])
+
+platforms = []
+
+def init_map():
+    global platforms
+    platforms.clear() # 맵 초기화
+
+    # ground
+    ground = PlatformData((0, 0, 800, 30), (100, 255, 100, 1), 'ground')
+    platforms.append(ground)
+
+    # floor_l_1 (one-way)
+    left, width = 100, 200
+    bottom, height = 120, 50
+    floor_l_1 = PlatformData((left, bottom, left+width, bottom+height), (100,255,150,1), 'one-way')
+    platforms.append(floor_l_1)
+
+    # floor_r_1 (one-way)
+    left, width = 400, 300
+    bottom, height = 100, 50
+    floor_r_1 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_r_1)
+
+    # floor_l_11 (one-way)
+    left, width = 30, 80
+    bottom, height = 200, 50
+    floor_l_11 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_l_11)
+
+    # floor_r_11 (one-way)
+    left, width = 650, 80
+    bottom, height = 185, 50
+    floor_r_11 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_r_11)
+
+    # floor_l_2 (one-way)
+    left, width = 50, 650
+    bottom, height = 280, 50
+    floor_2 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_2)
+
+    # floor_m_3 (one-way)
+    left, width = 180, 400
+    bottom, height = 400, 50
+    floor_m_3 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_m_3)
+
+    # floor_l_4 (one-way)
+    left, width = 100, 160
+    bottom, height = 520, 50
+    floor_l_4 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_l_4)
+
+    # floor_r_4 (one-way)
+    left, width = 500, 160
+    bottom, height = 520, 50
+    floor_r_4 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_r_4)
+
+    # floor_m_5 (one-way)
+    left, width = 180, 400
+    bottom, height = 620, 50
+    floor_m_5 = PlatformData((left, bottom, left + width, bottom + height), (100, 255, 150, 1), 'one-way')
+    platforms.append(floor_m_5)
+
+def get_platforms():
+    return platforms
+
+def draw_map():
+    for p in platforms:
+        draw_rectangle(*p.rect, *p.color, filled=True)
