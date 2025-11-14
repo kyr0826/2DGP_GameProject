87faf9a01f56eebd266b4c54c0abe6b8eadbe2f4
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-14 23:09:20 +0900

    용암과 플레이어 콜리션 이벤트 추가

--- Source/CollisionManager.py
+++ Source/CollisionManager.py
@@ -11,12 +11,23 @@ def collide(a_bb,b_bb):
     return True
 
 collision_pairs= {}
-def add_collision_pair(group, list_a, list_b):
+def add_list_collision_pair(group, list_a, list_b):
     if group not in collision_pairs:
         collision_pairs[group] = [[],[]]
 
-    collision_pairs[group][0] = list_a
-    collision_pairs[group][1] = list_b
+    if list_a:
+        collision_pairs[group][0] = list_a
+    if list_b:
+        collision_pairs[group][1] = list_b
+
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:
+        collision_pairs[group] = [[],[]]
+
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
 
 def handle_collisions():
     for group, pairs in collision_pairs.items():
@@ -31,16 +42,13 @@ def handle_collisions():
                 if group == 'player:map':
                     a_box = a.get_foot_bb()
                     b_box = b.rect
-                elif group == 'player:player':
+                elif group == 'player:player' or group == 'player:volcano':
                     a_box = a.get_bb()
                     b_box = b.get_bb()
                 elif group == 'attack:hit': # a -> b
-
                     if a.state_machine.current != States.ATTACK: continue
-
                     a_box = a.get_attack_bb()
                     b_box = b.get_bb()
-
                 if a_box and b_box and collide(a_box, b_box):
                     if group == 'attack:hit':
                         b.handle_collision('attack:hit', a)
