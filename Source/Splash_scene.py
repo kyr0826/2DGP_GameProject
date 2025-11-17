ccbbac120af46616b73afa6fca0a5db4eaf476de
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-18 03:30:37 +0900

    # splash_scene 변수명 변경 - logo_xxx -> splash_xxx

--- Source/Splash_scene.py
+++ Source/Splash_scene.py
@@ -2,33 +2,32 @@ from pico2d import *
 import game_framework
 import Lobby_scene
 
-logo_duration = 1.0
+splash_duration = 1.0
 
 
 def init():
-    global image, logo_start_time
+    global image, splash_start_time
 
     image = load_image('tuk_credit.png')
-    logo_start_time = get_time()
-
+    splash_start_time = get_time()
 
 def finish():
     global image
     del image
 
-
-def update():
-    global logo_start_time
-    if get_time() - logo_start_time >= logo_duration:
-        logo_start_time = get_time()
-        game_framework.change_mode(Lobby_scene)
-
+def handle_events(): pass
 
 def draw():
     clear_canvas()
     image.draw(400, 300)
     update_canvas()
 
+def update():
+    global splash_start_time
+    if get_time() - splash_start_time >= splash_duration:
+        splash_start_time = get_time()
+        game_framework.change_mode(Lobby_scene)
 
-def handle_events():
-    events = get_events()
+def pause(): pass
+
+def resume(): pass
--- Source/main.py
+++ Source/main.py
@@ -1,8 +1,8 @@
 from pico2d import open_canvas, close_canvas
 import game_framework
 
-import Loading_scene
+import Splash_scene
 
 open_canvas()
-game_framework.run(Loading_scene)
+game_framework.run(Splash_scene)
 close_canvas()
