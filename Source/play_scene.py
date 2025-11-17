f934ae8ea24b5f0c6f981da2b91baae09ea623ef
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-18 06:56:20 +0900

    플레이 씬에 용암 적용

--- Source/play_scene.py
+++ Source/play_scene.py
@@ -10,12 +10,20 @@ from Player_Info_bar import Player_Info_bar
 
 input_mgr = None
 p1_info, p2_info = None, None
+volcano_rising_timer = 0.0
+volcano = None
 
+time_frame_img : Image = None
+time_font : Font = None
 def init():
-    global input_mgr,p1_info,p2_info
+    global input_mgr,p1_info,p2_info, volcano_rising_timer, volcano
 
+    global time_frame_img, time_font
+    time_frame_img = load_image("UI/Time_Frame.png")
+    time_font = load_font("ENCR10B.TTF", size=24)
+
+    volcano_rising_timer = Volcano_rising_wait_time
     init_map()
-
 
     input_mgr = InputManager()
 
@@ -66,6 +74,8 @@ def handle_events():
                 game_framework.change_mode(Lobby_scene)
 
 def draw():
+    global time_frame_img, time_font
+
     clear_canvas()
 
     draw_map()
@@ -75,9 +85,29 @@ def draw():
     p1_info.draw()
     p2_info.draw()
 
+    time_frame_w = 140
+    time_frame_h = 40
+
+    time_frame_x = GAME_WINDOW_WIDTH // 2
+    time_frame_y = GAME_WINDOW_HEIGHT - time_frame_h//2 - 6
+
+    time_frame_img.draw(time_frame_x, time_frame_y, time_frame_w, time_frame_h)
+    minutes = int(volcano_rising_timer // 60)
+    seconds = int(volcano_rising_timer % 60)
+    time_text = f"{minutes:02d}:{seconds:02d}"
+    time_font.draw(time_frame_x - (len(time_text) * 24 * 0.28), time_frame_y,time_text,(255,125,0))
+
     update_canvas()
 
 def update():
+    global volcano_rising_timer
+
+    if not volcano.isLavaRising and volcano_rising_timer > 0 :
+        volcano_rising_timer -= game_framework.frame_time
+        if volcano_rising_timer <= 0 :
+            volcano_rising_timer = 0
+            volcano.isLavaRising = True
+
     game_world.update()
     handle_collisions()
 
