4934911563a5757376c73b5b05844512589e7ead
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-18 06:32:19 +0900

    캐릭터 정보 표시를 위한 클래스 생성

---
+++ Source/Player_Info_bar.py
@@ -0,0 +1,46 @@
+from Character import Character
+from pico2d import *
+import GameConstants
+from StateMachine import States
+
+class Player_Info_bar:
+    health_bar_frame : Image = None
+    ingame_face_frame : Image = None
+    character_face_images = {}
+
+    def __init__(self, owner:Character, isPlayer1):
+        self.owner = owner
+        self.isPlayer1 = isPlayer1
+
+        if not Player_Info_bar.health_bar_frame:
+            Player_Info_bar.health_bar_frame = load_image('UI/Health_Bar_Frame.png')
+
+        if not Player_Info_bar.ingame_face_frame:
+            Player_Info_bar.ingame_face_frame = load_image('UI/InGame_Face_Frame.png')
+
+        if not Player_Info_bar.character_face_images:
+            for name in GameConstants.characters:
+                Player_Info_bar.character_face_images[name] = load_image(name+'/select_face.png')
+
+    def draw(self):
+        # 선택된 캐릭터 얼굴
+        x = 45 if self.isPlayer1 else GameConstants.GAME_WINDOW_WIDTH - 45
+        y = GameConstants.GAME_WINDOW_HEIGHT - 50
+
+        Player_Info_bar.ingame_face_frame.draw(x,y,80,80)
+        Player_Info_bar.character_face_images[self.owner.name].draw(x, y, 60, 60)
+
+        # 체력바
+        y = y - 40
+        Player_Info_bar.health_bar_frame.draw(x,y, 70, 22)
+
+        if self.owner.state_machine.current is States.DEAD: return
+        healthbar_width = 60
+        health_rate = self.owner.Health / self.owner.MAX_HEALTH
+        left = x - healthbar_width // 2
+        bottom = y - 6
+
+        right = left + healthbar_width * health_rate
+        top = y + 6
+
+        draw_rectangle(left, top, right, bottom, 255, 100, 100, 255, True)
