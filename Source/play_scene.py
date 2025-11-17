ee4d8d1e7de1f8a42c1cc4d497dd34012e507262
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-18 06:32:35 +0900

    플레이 씬에 Player_Info_bar 적용

--- Source/play_scene.py
+++ Source/play_scene.py
@@ -6,12 +6,16 @@ from Volcano import Volcano
 from Source.CollisionManager import add_collision_pair, add_list_collision_pair, handle_collisions, collision_pairs
 import Lobby_scene
 from GameConstants import *
+from Player_Info_bar import Player_Info_bar
+
 input_mgr = None
+p1_info, p2_info = None, None
 
 def init():
-    global input_mgr
+    global input_mgr,p1_info,p2_info
 
     init_map()
+
 
     input_mgr = InputManager()
 
@@ -19,10 +23,14 @@ def init():
                         {'left': SDLK_a, 'right': SDLK_d, 'jump': SDLK_w, 'down': SDLK_s,
                          'attack': SDLK_g, 'defense': SDLK_h, 'parrying': SDLK_j})
 
+    p1_info = Player_Info_bar(player1,True)
+
+
     player2 = Character(selected_characters['player2'], 500, 300, input_mgr,
                         {'left': SDLK_LEFT, 'right': SDLK_RIGHT, 'jump': SDLK_UP, 'down': SDLK_DOWN,
                          'attack': SDLK_KP_1, 'defense': SDLK_KP_2, 'parrying': SDLK_KP_3})
 
+    p2_info = Player_Info_bar(player2, False)
 
     volcano = Volcano()
     game_world.add_object(volcano,1)
@@ -59,8 +67,14 @@ def handle_events():
 
 def draw():
     clear_canvas()
+
     draw_map()
+
     game_world.render()
+
+    p1_info.draw()
+    p2_info.draw()
+
     update_canvas()
 
 def update():
