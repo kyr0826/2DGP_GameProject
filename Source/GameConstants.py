b79c0724c52f8f99c36f23fb043eeaf6deff3794
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-18 05:59:41 +0900

    게임 상수 따로 빼기

---
+++ Source/GameConstants.py
@@ -0,0 +1,15 @@
+GAME_WINDOW_WIDTH = 800
+GAME_WINDOW_HEIGHT = 750
+
+selected_characters = {
+    'player1': None,
+    'player2': None
+}
+
+characters = ['Fighter', 'Samurai', 'Shinobi']
+
+p1_index = 0
+p2_index = 0
+
+p1_selected = False
+p2_selected = False
