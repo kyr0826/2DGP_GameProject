65a866268e4717304a1923c0ddc690a82dc893ec
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-11-30 01:03:19 +0900

    맵 카드 잘 적용되는지 테스트

--- Source/main.py
+++ Source/main.py
@@ -2,7 +2,8 @@ from pico2d import open_canvas, close_canvas
 import game_framework
 
 import Splash_scene
+import map_select__scene
 
 open_canvas(800,750)
-game_framework.run(Splash_scene)
+game_framework.run(map_select__scene)
 close_canvas()
