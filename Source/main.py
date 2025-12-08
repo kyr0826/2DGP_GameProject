cc2e6e784ea681da074bdd208ed4cbc14ce55dbe
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-12-09 01:03:52 +0900

    main에서 사운드 리소스 초기화 함수 실행

--- Source/main.py
+++ Source/main.py
@@ -1,8 +1,10 @@
 from pico2d import open_canvas, close_canvas
 import game_framework
-
 import Splash_scene
+import GameSounds as gs
 
 open_canvas(800,750)
+gs.init()
+
 game_framework.run(Splash_scene)
 close_canvas()
